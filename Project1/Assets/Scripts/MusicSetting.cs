8f01dcf0f11f5adce5a1b5a6f37be5fb77371f57
Naeem Meman <naeemmeman@gmail.com>
2020-05-09 16:00:22 -0400

    Testing to see if this works

---
+++ Project1/Assets/Scripts/MusicSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSetting : MonoBehaviour
+{
+	private AudioSource audioSrc;
+	
+	void Start()
+	{
+		audioSrc = GetComponent<AudioSource>();
+	}
+	
+	void Update()
+	{
+		audioSrc.volume = GlobalManager.volume;
+	}
+	
+	public void SetVolume(float vol)
+	{
+		GlobalManager.volume = vol;
+		audioSrc.volume = GlobalManager.volume;
+	}
+
+}
